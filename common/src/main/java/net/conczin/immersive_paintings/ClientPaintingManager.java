293e44088c8e099aac26a6454493710b347287ed
USS-LongIsland <longislanduss@outlook.com>
2026-07-16 03:03:45 +0000

    Fix: use daemon threads for ClientPaintingManager executor service

--- common/src/main/java/net/conczin/immersive_paintings/ClientPaintingManager.java
+++ common/src/main/java/net/conczin/immersive_paintings/ClientPaintingManager.java
@@ -28,7 +28,11 @@ public class ClientPaintingManager {
     // We don't need to cache entries in memory on the client, the value is always immediately registered as a texture
     private static final ClientCache paintingCache = new ClientCache(0);
 
-    private final static ExecutorService service = Executors.newFixedThreadPool(2);
+    private final static ExecutorService service = Executors.newFixedThreadPool(2, r -> {
+        Thread t = new Thread(r);
+        t.setDaemon(true);
+        return t;
+    });
 
     public static void newTexture(Identifier location, BufferedImage data) {
         Minecraft.getInstance().execute(() -> {
